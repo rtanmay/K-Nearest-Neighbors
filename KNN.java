a05981c2843047f56586ef90f15a34fd01844920
rtanmay <rtanmay05@gmail.com>
2018-07-26 16:57:06 +0530

    Updated dataset and fixed bugs

--- KNN.java
+++ KNN.java
@@ -5,19 +5,19 @@ import java.util.Map.Entry;
 public class KNN
 {
 	/*
-	 *Dataset- 5 datapoints, each datapoint has 3 attributes
+	 *Dataset- 20 datapoints, each datapoint has 3 attributes (Modified IRIS Dataset)
 	 */
-	static double[] att1 = {2.2, 3.3, 4, 8.9, 2.5};
-	static double[] att2 = {3.2, 4.3, 5.2, 6.2 ,7.4}; 
-	static String[] att3 = {"red", "orange", "yellow", "orange", "orange"};
-	static int[] cls  = {1, 2, 1, 2, 2}; //This is class
+	static double[] att1 = {5.1, 4.9, 4.7, 4.6, 5.0, 5.4,4.6,5.0,4.4,4.9, 7.0,6.4,6.9,5.5,6.5,5.7,6.3,4.9,6.6,5.2};
+	static double[] att2 = {1.4, 1.4, 1.3, 1.5, 1.4, 1.7,1.4,1.5,1.4,1.5, 4.7,4.5,4.9,4.0,4.6,4.5,4.7,3.3,4.6,3.9}; 
+	static String[] att3 = {"orange","orange","orange","orange","orange","orange","orange","orange","orange","orange","yellow","yellow","yellow","yellow","yellow","yellow","yellow","yellow","yellow","yellow"};
+	static int[] cls  = {1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2}; //This is class
 	static int k,dval;
 	static double a1,a2;
 	static String a3;
-	static double[] d1= new double[5];
-	static double[] d2= new double[5];
-	static int[] d3= new int[5];
-	static double[] finald=new double[5];
+	static double[] d1= new double[20];
+	static double[] d2= new double[20];
+	static int[] d3= new int[20];
+	static double[] finald=new double[20];
 	static int[] countclass=new int[2];
 	static int finalclass;
 	static HashMap<Double, Integer> hm= new HashMap<Double, Integer>();
@@ -26,8 +26,13 @@ public class KNN
 	{
 		Scanner sc=new Scanner(System.in);
 		
-		System.out.println("Enter the value of k you wish to take in KNN: ");
+		System.out.println("Enter the value of k you wish to take in KNN(0 for default): ");
 		k=sc.nextInt();
+		if(k==0)
+		{
+			k=(int)(Math.sqrt(20));
+		}
+		//System.out.println("This is k=" + k);
 		System.out.println("Enter value of attribute 1: ");
 		a1=sc.nextDouble();
 		System.out.println("Enter value of attribute 2: ");
@@ -42,7 +47,7 @@ public class KNN
 	{
 		if(dval==1) //Euclidean distance
 		{
-			for(int i=0;i<5;i++)
+			for(int i=0;i<20;i++)
 			{
 				d1[i]=Math.sqrt( Math.abs(a1*a1 - att1[i]*att1[i]) );
 				d2[i]=Math.sqrt( Math.abs(a2*a2 - att2[i]*att2[i]) );
@@ -50,7 +55,7 @@ public class KNN
 		}
 		else //Manhattan distance
 		{
-			for(int i=0;i<5;i++)
+			for(int i=0;i<20;i++)
 			{
 				d1[i]=Math.abs(a1-att1[i]);
 				d2[i]=Math.abs(a2-att2[i]);
@@ -58,7 +63,7 @@ public class KNN
 		}
 
 		//Hamming distance
-		for(int i=0;i<5;i++)
+		for(int i=0;i<20;i++)
 		{
 			int count=0;
 			for(int j=0;j<Math.min(att3[i].length(),a3.length());j++)
@@ -74,22 +79,27 @@ public class KNN
 
 	public static void selectk()
 	{
-		for(int i=0;i<5;i++)
+		for(int i=0;i<20;i++)
 		{
 			finald[i]=d1[i]+d2[i]+d3[i];
 		}
 		//Storing in Hashmap
-		for(int i=0;i<5;i++)
+		for(int i=0;i<20;i++)
 		{
 			hm.put(finald[i],cls[i]);
 		}
 		TreeMap<Double,Integer> sorted= new TreeMap<>(hm);
 		Set<Entry<Double, Integer> > aftersort= sorted.entrySet();
 
+		int i=0;
 		for(Entry<Double,Integer> x:aftersort)
 		{
+			if(i==k) break;
 			countclass[x.getValue()-1]++;
+			i++;
 		}
+		//System.out.println("This is countclass[0]=" + countclass[0]);
+		//System.out.println("This is countclass[1]=" + countclass[1]);
 		if(countclass[0]>=countclass[1])
 		{
 			finalclass=1;
